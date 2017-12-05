a8cf207d9c3d78085c355184cd9105d31c32c49c
David Michaeloff <davidmichaeloff@Davids-Mac-mini.local>
2017-12-04 18:22:47 -0800

    Adding new file to branch.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by David Michaeloff on 12/4/17.
+//  Copyright © 2017 David Michaeloff. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
